a6f0f70106d08b05c0c906643514eb7fce02a223
FranValderrey <fran.valderrey@gmail.com>
2024-03-17 18:02:02 +0100

    Commit 4.5: Creación de middleware de gestión de errores y 404, y del lanzador de la app

--- server.js
+++ server.js
@@ -4,3 +4,31 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+const { PORT } = process.env;
+
+app.use(morgan('dev'));
+
+// Rutas
+
+// Middleware de 404
+app.use((req, res) => {
+  res.status(404).send({
+    status: 'error',
+    message: 'Not found',
+  });
+});
+
+// Middleware de gestión de errores
+app.use((error, req, res, next) => {
+  console.error(error);
+
+  res.status(error.httpStatus || 500).send({
+    status: 'error',
+    message: error.message,
+  });
+});
+
+// Lanzador del servidor
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT} ❤️`);
+});
